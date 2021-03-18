6e8f14b52ae039f6c93f871009bff65a71f547d1
Puli Gangadhara <gangadharapuli94@gmail.com>
2021-03-18 14:42:33 +0530

    added hungry prompt

--- src/Hungry.java
+++ src/Hungry.java
@@ -1,7 +1,13 @@
-
+import java.util.Scanner;
 public class Hungry {
 	public static void main(String[] args) {
-		System.out.println("Hungry");
+		System.out.println("are you Hungry??");
+		Scanner sc=new Scanner(System.in);
+		String Answer=sc.nextLine();
+		if(Answer.equals("Yes"))
+			System.out.println("Eat Samosa");
+		else
+			System.out.println("What did you eat?");
 	}
 
 }
